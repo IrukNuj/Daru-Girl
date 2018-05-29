5ee62298baecd4f067df1d8e122c46bc18c47cec
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-05-29 22:14:35 +0900

    daruko

--- tweet.rb
+++ tweet.rb
@@ -7,17 +7,17 @@ class Tweet
 
   def initialize
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = MY_CONSUMER_KEY
-      config.consumer_secret     = MY_CONSUMER_SECRET
-      config.access_token        = MY_ACCESS_TOKEN
-      config.access_token_secret = MY_ACCESS_TOKEN_SECRET
+      config.consumer_key        = "2nSXrucAvvI9XKxjQcZUn0uFX"
+      config.consumer_secret     = "1hudid8U6Z9Z2Gj6EeHV28G04ni0zVeMAPcuWUfFRGUOVX1WvI"
+      config.access_token        = "997858584699420672-iM8qSdOtoLXaXWWho058urWuP2Kci47"
+      config.access_token_secret = "RTNXv3KAdDSHX1YO9sLiOdKijvKOEVGKTfFdFd8Gv6wBr"
     end
 
     @stream_client = Twitter::Streaming::Client.new do |config|
-      config.consumer_key        = MY_CONSUMER_KEY
-      config.consumer_secret     = MY_CONSUMER_SECRET
-      config.access_token        = MY_ACCESS_TOKEN
-      config.access_token_secret = MY_ACCESS_TOKEN_SECRET
+      config.consumer_key        = "2nSXrucAvvI9XKxjQcZUn0uFX"
+      config.consumer_secret     = "1hudid8U6Z9Z2Gj6EeHV28G04ni0zVeMAPcuWUfFRGUOVX1WvI"
+      config.access_token        = "997858584699420672-iM8qSdOtoLXaXWWho058urWuP2Kci47"
+      config.access_token_secret = "RTNXv3KAdDSHX1YO9sLiOdKijvKOEVGKTfFdFd8Gv6wBr"
     end
   end
 
