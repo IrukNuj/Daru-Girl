ee0f06215ee009eb872e7eaa032b2a89d13662c6
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-06-04 17:14:53 +0900

    test

--- tweet.rb
+++ tweet.rb
@@ -8,11 +8,17 @@ class Tweet
 
   def initialize
     @client = Twitter::REST::Client.new do |config|
-
+      config.consumer_key        = "2nSXrucAvvI9XKxjQcZUn0uFX"
+      config.consumer_secret     = "1hudid8U6Z9Z2Gj6EeHV28G04ni0zVeMAPcuWUfFRGUOVX1WvI"
+      config.access_token        = "997858584699420672-iM8qSdOtoLXaXWWho058urWuP2Kci47"
+      config.access_token_secret = "RTNXv3KAdDSHX1YO9sLiOdKijvKOEVGKTfFdFd8Gv6wBr"
     end
 
     @stream_client = Twitter::Streaming::Client.new do |config|
-
+      config.consumer_key        = "2nSXrucAvvI9XKxjQcZUn0uFX"
+      config.consumer_secret     = "1hudid8U6Z9Z2Gj6EeHV28G04ni0zVeMAPcuWUfFRGUOVX1WvI"
+      config.access_token        = "997858584699420672-iM8qSdOtoLXaXWWho058urWuP2Kci47"
+      config.access_token_secret = "RTNXv3KAdDSHX1YO9sLiOdKijvKOEVGKTfFdFd8Gv6wBr"
     end
   end
 
