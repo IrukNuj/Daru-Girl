86fa0cdda186d6d509e9cd051391ae49392c0dad
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-06-04 18:54:37 +0900

    tes

--- app.rb
+++ app.rb
@@ -3,11 +3,6 @@ require_relative 'tweet.rb'
 
 # URL'/'でアクセス
 get '/' do
-  "Streaming..."
-  Tweet.new.stream_reply
-end
-
-get '/random_tweet' do
   Tweet.new.random_tweet
   "Tweet successd!"
 end
