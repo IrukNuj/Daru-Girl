f658d765ac7bc7e8669b84c41d0159702963a171
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-06-04 19:38:35 +0900

    a

--- tweet.rb
+++ tweet.rb
@@ -15,7 +15,7 @@ class Tweet
     end
 
     @stream_client = Twitter::Streaming::Client.new do |config|
-      config.consumer_key        = ENV['MY_CONSUMER_KEY']
+      config.consumer_key        = ENV['MY_CONSUMER']
       config.consumer_secret     = ENV['MY_CONSUMER_SECRET']
       config.access_token        = ENV['MY_ACCESS_TOKEN']
       config.access_token_secret = ENV['MY_ACCESS_TOKEN_SECRET']
