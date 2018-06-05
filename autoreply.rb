efe2b81a3d374259e2d8159db1fe5af5f09365d2
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-06-05 17:17:12 +0900

    リプ用とsinatraで分割

--- autoreply.rb
+++ autoreply.rb
@@ -1,3 +1,45 @@
-require_relative './tweet'
+require 'twitter'
+require 'date'
+require 'dotenv'
+require_relative './reply'
 
-Tweet.new.stream_reply
+USERNAME = "Daru_Girl"
+
+class Tweet
+
+  def initialize
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['CONSUMER_KEY']
+      config.consumer_secret     = ENV['CONSUMER_SECRET']
+      config.access_token        = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+    end
+    
+    @stream_client = Twitter::Streaming::Client.new do |config|
+      config.consumer_key        = ENV['CONSUMER_KEY']
+      config.consumer_secret     = ENV['CONSUMER_SECRET']
+      config.access_token        = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+    end
+  end
+
+  def stream_reply
+    morning_time = (4..10)
+    day_time = (11..15)
+    evening_time = (16..18)
+    night_time = [*0..3],[*19..24]
+
+    @stream_client.user do |tweet|
+      if tweet.is_a?(Twitter::Tweet)
+        reply_ohayo(tweet)
+        reply_tsukareta(tweet)
+        reply_oyasumi(tweet)
+        reply_daruko(tweet)
+        reply_ganbaru(tweet)
+        @client.favorite(tweet.id) if rand(1..10) > 9
+      end #if
+    end #stream
+
+  end#def
+
+end
